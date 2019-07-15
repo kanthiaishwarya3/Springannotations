09c57fd13ab226d61e62cda4272e868578bc93dc
aishwarya <pappulaaishwarya@gmail.com>
2019-07-15 09:53:44 +0530

    submitted spring annotations

---
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -0,0 +1,37 @@
+
+
+package com.stackroute.domain;
+import org.springframework.beans.factory.annotation.Value;
+
+public class Actor {
+    @Value("Rajini Kanth")
+    private String name;
+    @Value("Male")
+    private String gender;
+    @Value("65")
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
